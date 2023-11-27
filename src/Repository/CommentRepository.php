c82c876d602b7a43d89cc4842a7f751f180734c1
Marchenko Sergey <mse08061993@gmail.com>
2023-11-27 17:59:00 +0300

    Add command to remove old spam or rejected comments

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -7,10 +7,12 @@ use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\ORM\QueryBuilder;
 
 class CommentRepository extends ServiceEntityRepository
 {
     public const COMMENTS_PER_PAGE = 2;
+    private const DAYS_BEFORE_REJECTED_REMOVAL = 7;
 
     public function __construct(ManagerRegistry $registry)
     {
@@ -33,5 +35,30 @@ class CommentRepository extends ServiceEntityRepository
         ;
 
         return new Paginator($query);
+    }
+
+    public function countOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->select('COUNT(c.id)')->getQuery()->getSingleScalarResult();
+    }
+
+    public function deleteOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
+    }
+
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        $builder = $this->createQueryBuilder('c')
+            ->andWhere('c.state = :spam_state or c.state = :rejected_state')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters([
+                'spam_state' => 'spam',
+                'rejected_state' => 'rejected',
+                'date' => new \DateTimeImmutable(-self::DAYS_BEFORE_REJECTED_REMOVAL . ' days')
+            ])
+        ;
+
+        return $builder;
     }
 }
